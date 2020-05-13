4d161c8739a5ebf67ec61771915984b97b7fa583
Stephen Kissler <stephen.kissler@gmail.com>
2020-05-13 11:41:59 -0400

    new

---
+++ classsim.R
@@ -0,0 +1,6 @@
+# 2 people
+# 0 for uninfected, 1 for infected
+# 4 possibilities
+
+from <- list(c(0,0),c(0,1),c(1,0),c(1,1))
+to <- list(list(c(0,0)), list(c(0,1),c(1,1)), list(c(1,1),c(1,0)), list(c(1,1))) 
