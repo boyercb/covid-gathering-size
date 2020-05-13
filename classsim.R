d330fb8f9e7619fbe4aa17f2598f3fb69e0c0d8e
Stephen Kissler <stephen.kissler@gmail.com>
2020-05-13 11:49:08 -0400

    new

--- classsim.R
+++ classsim.R
@@ -11,18 +11,18 @@ to <- list(list(c(0,0)), list(c(0,1),c(1,1)), list(c(1,1),c(1,0)), list(c(1,1)))
 # 11
 
 # Prior prevalence of infectiousness:
-prev <- 0.1 
+p <- 0.1 
 q <- 0.2 # Probability of transmission
 
 
 # Initialize a probability transition matrix
-transmat <- matrix(rep(0,16), nrow=4)
 
+priorprob <- c((1-p)^2, 2*p*(1-p), 2*p*(1-p), p^2)
 
-t(matrix(c(
+transmat <- t(matrix(c(
 c(1,0,0,0),
 c(0,1-q,0,q),
 c(0,0,1-q,q),
 c(0,0,0,1)
-),ncol=4)
+),ncol=4))
 
