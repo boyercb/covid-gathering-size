ebe3b8c7e07ba0b2474725646d144e7facf884f1
Stephen Kissler <stephen.kissler@gmail.com>
2020-05-13 12:14:59 -0400

    new figure

--- classsim.R
+++ classsim.R
@@ -1,3 +1,5 @@
+library(tidyverse)
+
 # =============================================================================
 # Group size 2 
 # =============================================================================
@@ -22,6 +24,7 @@ c(0,0,1-q,q),
 c(0,0,0,1)
 ),ncol=4))
 
+priorprob*colSums(transmat)
 
 # =============================================================================
 # Group size 3 
@@ -38,6 +41,15 @@ c(0,0,0,1)
 p <- 0.1 # Prior prevalence of infectiousness:
 q <- 0.2 # Probability of transmission
 
+prng <- seq(0.1, 0.9, 0.1)
+
+outcomevec0 <- rep(NA,length(prng))
+outcomevec1 <- rep(NA,length(prng))
+outcomevec2 <- rep(NA,length(prng))
+outcomevec3 <- rep(NA,length(prng))
+
+for(indexA in 1:length(prng)){
+	p <- prng[indexA]
 priorprob <- c(
 	(1-p)^3,
 	3*p*(1-p)^2,
@@ -53,12 +65,22 @@ transmat <- t(matrix(c(
 ),nrow=4))
 
 
+outcome <- priorprob*colSums(transmat)
+outcomevec0[indexA] <- outcome[1]
+outcomevec1[indexA] <- outcome[2]
+outcomevec2[indexA] <- outcome[3]
+outcomevec3[indexA] <- outcome[4]
+}
 
+df <- data.frame(p=prng, out0=outcomevec0,out1=outcomevec1,out2=outcomevec2,out3=outcomevec3)
 
+fig3 <- df %>%
+	pivot_longer(cols=c("out0","out1","out2","out3"), names_to="nout", values_to="values") %>%
+	ggplot(aes(x=p, y=values, col=nout))+ 
+		geom_point() + 
+		geom_line()
 
-
-
-
+ggsave(df, file="~/Desktop/")
 
 
 
