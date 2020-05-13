0f51f17d47751b784e004473462f62d19d5cccda
Stephen Kissler <stephen.kissler@gmail.com>
2020-05-13 11:43:37 -0400

    new

--- classsim.R
+++ classsim.R
@@ -4,3 +4,11 @@
 
 from <- list(c(0,0),c(0,1),c(1,0),c(1,1))
 to <- list(list(c(0,0)), list(c(0,1),c(1,1)), list(c(1,1),c(1,0)), list(c(1,1))) 
+
+# 00
+# 01
+# 10
+# 11
+
+# Initialize a probability transition matrix
+transmat <- matrix(rep(0,16), nrow=4)
