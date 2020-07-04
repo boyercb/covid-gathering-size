a892cd28c23d4961b806964653c0ca7603ec8547
boyercb <christopherbboyer@gmail.com>
2020-07-03 23:25:52 -0400

    Update heterogeneous_q.R

--- heterogeneous_q.R
+++ heterogeneous_q.R
@@ -1,100 +1,149 @@
-# This function simulates a gathering of size N drawn from a source population
-# in which the prevalence of susceptibility and infection are given by ps and pi
-# respectively. The function also allows us to specify the distribution for nu.
-dgp <- function(N,
-                ps,
-                pi,
-                T = 1,
-                D = 10,
-                nu_dist = function(x)
-                  rgamma(x, 0.45, 0.58)) {  # <-- This can be changed to whatever distribution we want!
-  # draw random sample of attendees from population of
-  # 1. susceptibles 
-  # 2. infecteds
-  # 3. recovereds
-  y <- rmultinom(1, N, c(ps, pi, 1 - ps - pi))
-  y <- as.vector(y)
-  
-  # rename to reflect compartments
-  names(y) <- c("S", "I", "R")
-  
-  # draw infectiousness (nu) for each infected
-  nu <- nu_dist(y["I"]) 
-  
-  # calculate probability of infection assuming homogeneous mixing
-  q <- 1 - exp(-sum(nu) * (T/D))
-  
-  # flip S q-probability coins to determine number who get infected
-  delta <- rbinom(1, y["S"], q)
-  
-  # return results
-  return(list(
-    "y" = y,
-    "q" = q,
-    "delta" = delta
-  ))
-}
+library(tidyverse)
+library(deSolve)
+
+source("simulation_functions.R")
 
 
-library(ggplot2)
+# simulation globals ------------------------------------------------------
 
-df <- data.frame(
-  nu = rgamma(1000, 0.45, 0.58)
-)
+SIMS <- 10000
 
-ggplot(df, aes(x = nu)) + 
-  geom_histogram()
+N_SEQ <- seq(2, 100)               # size of gatherings
+PI_SEQ <- seq(0.001, 0.09, 0.001)  # population prevalence of infection
+PS_SEQ <- 1 - PI_SEQ               # population prevalence of susceptibles
+                                   # (NOTE: for now assume no recovereds)  
 
-df <- 
-expand.grid(
-  N = seq(2, 100),
-  pi = seq(0.001, 0.09, 0.001)
-)
+PHI_SEQ <- seq(0.2, 1, 0.2)        # dispersion parameter 
+R0_SEQ <- seq(0.5, 2.5, 0.5)       # mean number of secondary cases
 
-df$delta <-
-  map2_dbl(df$N,
-       df$pi,
-       function(x, y) {
-         replicate(1000, {
-           sim <- dgp(x, 0.9, y)
-           sim$delta
-         }) %>% mean()
-       })
   
-ggplot(filter(df, N <= 20), aes(x = N, y = pi, z = delta)) +
-  geom_contour_filled(binwidth = 1)
-  
+# simulation 1 - use HK parameters, vary N and pi -------------------------
 
-df <- 
+sim1_params <-
   expand.grid(
-    N = seq(2, 20),
-    pi = seq(0.001, 0.09, 0.001),
-    phi = seq(0.1, 0.5, 0.1),
-    r0 = seq(0.5, 2.5, 0.5)
+    N = N_SEQ,
+    pi = PI_SEQ
   )
+
+sim1_params$ps <- 1 - sim1_params$pi  
+
+sim1_results <-
+  pmap_dfr(
+    as.list(sim1_params),
+    function(N, ps, pi) {
+      sim <- dgp(N, ps, pi, sims = SIMS)
+      summarize_dgp(sim)
+    }
+  )
+
+sim1 <- cbind(sim1_params, sim1_results)
+
+
+# simulation 2 - now also vary phi and r0 ---------------------------------
+
+sim2_params <- 
+  expand.grid(
+    N = N_SEQ,
+    pi = PI_SEQ,
+    phi = PHI_SEQ,
+    r0 = R0_SEQ
+  )
+
+sim2_params$ps <- 1 - sim2_params$pi  
 
 pb <- txtProgressBar(max = nrow(df), initial = NA, style = 3)
 
-df$delta <-
-  pmap_dbl(
-  list(
-    N = df$N,
-    pi = df$pi,
-    phi = df$phi,
-    r0 = df$r0
-  ),
-  function(N, pi, phi, r0) {
-    i <- getTxtProgressBar(pb)
-    setTxtProgressBar(pb, ifelse(is.na(i), 1, i + 1))
-    replicate(1000, {
-      sim <- dgp(N, 0.9, pi, nu_dist = function(x) rgamma(x, phi, r0))
-      sim$delta
-    }) %>% mean()
-  })
+sim2_results <-
+  pmap_dfr(
+    as.list(sim2_params),
+    function(N, ps, pi, phi, r0) {
+      
+      i <- getTxtProgressBar(pb)
+      setTxtProgressBar(pb, ifelse(is.na(i), 1, i + 1))
+      
+      sim <-
+        dgp(
+          N = N,
+          ps = ps,
+          pi = pi,
+          sims = SIMS,
+          nu_dist = function(x)
+            rgamma(length(x), x * phi, phi /  r0)
+        )
+      
+      summarize_dgp(sim)
+    })
 
-ggplot(df, aes(x = N, y = pi, z = delta)) +
-  facet_grid(r0 ~ phi) +
-  geom_contour_filled(binwidth = 1)
+close(pb)
 
+sim2 <- cbind(sim2_params, sim2_results)
+
+
+# Plots -------------------------------------------------------------------
+
+# nicely formatted labels for plotting
+r0_labs <- c(
+  "0.5" = bquote(R[0] == 0.5),
+  "1" = bquote(R[0] == 1),
+  "1.5" = bquote(R[0] == 1.5),
+  "2" = bquote(R[0] == 2),
+  "2.5" = bquote(R[0] == 2.5)
+)
+
+phi_labs <- c(
+  "0.2" = bquote(phi == 0.2),
+  "0.4" = bquote(phi == 0.4),
+  "0.6" = bquote(phi == 0.6),
+  "0.8" = bquote(phi == 0.8),
+  "1" = bquote(phi == 1)
+)
+
+# Plot 1: using HK parameters what is the expected number of secondary cases
+# when varying N and pi
+ggplot(sim1, aes(x = N, y = pi, fill = mean)) +
+  geom_tile() +
+  scale_fill_viridis_b(
+    name = bquote("Expected\nnew cases E["*Delta*I*"]"),
+    breaks = seq(1, 35, 1)
+  ) +
+  coord_cartesian(expand = F) +
+  labs(
+    x = "Size of gathering (N)",
+    y = bquote("Prevalence of infection ("*p[i]*")")
+  ) +
+  guides(fill=guide_legend(ncol = 2))
+
+
+# Plot 2: additionally vary dispersion and mean
+ggplot(sim2, aes(x = N, y = pi, fill = mean)) +
+  facet_grid(factor(r0, labels = r0_labs) ~ factor(phi, labels = phi_labs),
+             labeller = label_parsed) +
+  geom_tile() +
+  scale_fill_viridis_c(
+    name = bquote("Expected\nnew cases E[" * Delta * I * "]")
+  ) +
+  coord_cartesian(expand = F) +
+  labs(
+    x = "Size of gathering (N)",
+    y = bquote("Prevalence of infection (" * p[i] * ")")
+  ) +
+  guides(fill=guide_legend(ncol = 2))
+
+
+# Plot 3: check distributional effects in secondary cases
+ggplot(sim2, aes(x = N, y = pi, fill = sd)) +
+  facet_grid(factor(r0, labels = r0_labs) ~ factor(phi, labels = phi_labs),
+             labeller = label_parsed) +
+  geom_tile() +
+  scale_fill_viridis_b(
+    name = bquote("SD new\ncases SD[" * Delta * I * "]"),
+    breaks = seq(1, 35, 1)
+  ) +
+  coord_cartesian(expand = F) +
+  labs(
+    x = "Size of gathering (N)",
+    y = bquote("Prevalence of infection (" * p[i] * ")")
+  ) +
+  guides(fill=guide_legend(ncol = 2))
 
 
