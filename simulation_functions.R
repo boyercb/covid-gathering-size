2286dfb0298cc3e8c3d98712e3c98d8aaa47fa21
boyercb <christopherbboyer@gmail.com>
2020-07-10 12:21:05 -0400

    clean up simulation functions

--- simulation_functions.R
+++ simulation_functions.R
@@ -9,7 +9,6 @@ dgp <- function(N,
                 T = 1,
                 D = 10,
                 sims = 1,
-                return_SIR = FALSE,
                 nu_dist = function(x)
                   rgamma(x, 0.16, 0.16 / 2.5)) {  # <-- This can be changed to whatever distribution we want!
   # draw random sample of attendees from population of
@@ -17,81 +16,82 @@ dgp <- function(N,
   # 2. infecteds
   # 3. recovereds
   if (ps + pi == 1) {
-    y <- rmultinom(sims, N, c(ps, pi))
-    y <- rbind(y, 0)
+    mat <- rmultinom(sims, N, c(ps, pi))
+    mat <- rbind(mat, 0)
   } else {
-    y <- rmultinom(sims, N, c(ps, pi, 1 - ps - pi))
+    mat <- rmultinom(sims, N, c(ps, pi, 1 - ps - pi))
   }
   
   # transpose so rows are sims
-  y <- t(y)
+  mat <- t(mat)
   
   # rename to reflect compartments
-  colnames(y) <- c("S", "I", "R")
+  colnames(mat) <- c("S", "I", "R")
   
-  # draw infectiousness (nu) for each infected and use poisson to get expected r
-  r <- mapply(sim_infections, N = N, S = y[, "S"], I = y[, "I"], R = y[, "R"])
+  # draw infectiousness (nu) for each infected and simulate sequence of
+  # infections to get effective r for each infected]
+  r <-
+    mapply(
+      sim_infections,
+      N = N,
+      S = mat[, "S"],
+      I = mat[, "I"],
+      R = mat[, "R"],
+      MoreArgs = list(nu_dist = nu_dist)
+    )
   
-  # flip S q-probability coins to determine number who get infected
+  # calculate total number infected at gathering
   delta <- sapply(r, sum)
+  mat <- cbind(mat, delta)
 
   # return results
-  if (return_SIR) {
-    return(list(
-      "y" = y,
-      "r" = unlist(r),
-      "delta" = delta
-    ))
-  } else {
-    return(delta)
-  }
+  res <- list(
+    "SIR" = mat,
+    "r" = unlist(r),
+    "r_eff" = unlist(r[which(mat[, "I"] > 0)]), 
+    "delta" = delta
+  )
+  
+  return(res)
 }
 
-# This function simulates a gathering of size N drawn from a source population
-# in which the prevalence of susceptibility is given by ps. The function also
-# allows us to specify the distribution for nu.
-dgp_r <- function(N,
-                  ps,
-                  T = 1,
-                  D = 10,
-                  sims = 1,
-                  return_SIR = FALSE,
-                  nu_dist = function(x)
-                    rgamma(length(x), x * 0.45, 0.45 / 0.58)) {
-
-  if (ps < 1) { # if there are immune types
-    # draw random sample of attendees from population of
-    # 1. susceptibles 
-    # 2. recovereds
-    y <- rmultinom(sims, N, c(ps, 1 - ps))
-    y <- rbind(y[1, ], 0, y[2, ])
-    y <- t(y)
-  } else { # otherwise fix 1 infected and N - 1 recored
-    y <- matrix(rep(c(N - 1, 1, 0), each = sims), nrow = sims, ncol = 3)
-  }
+# This is a helper function that draws individual infectiousness and simulates
+# infection process at a gathering based on number in each initial disease state
+# and the right prior distribution for infectiveness
+sim_infections <- function(N,
+                           S,
+                           I,
+                           R,
+                           nu_dist = function(x)
+                             rgamma(x, 0.16, 0.16 / 2.5)) {
   
-  # rename to reflect compartments
-  colnames(y) <- c("S", "I", "R")
-  
-  # draw infectiousness (nu) for each infected
-  sum_nu <- nu_dist(y[, "I"])
-  
-  # calculate probability of infection assuming homogeneous mixing
-  q <- 1 - exp(-sum_nu * (T/D))
-  
-  # flip S q-probability coins to determine number who get infected
-  delta <- rbinom(sims, y[, "S"], q)
-  
-  # return results
-  if (return_SIR) {
-    return(list(
-      "y" = y,
-      "q" = q,
-      "delta" = delta
-    ))
+  if (I != 0) {
+    # draw number to infect using hierarchical poisson approach
+    r_i <- rpois(I, nu_dist(I))
+    
+    # cap maximum possible secondary infections at size of gathering minus 1
+    r_i <- ifelse(r_i > N-1, N-1, r_i)
+    
+    # initialize counters
+    n_s <- S
+    n_e <- 0
+    n_i <- I
+    n_r <- R
+    r_eff <- vector()
+    
+    # loop over infecteds and determine number of successful secondary cases
+    # using draw from hypergeometric distribution where m = # of remaining
+    # susceptible (success state) and n = # of recovered, exposed or already
+    # infected (failure state)
+    for (i in 1:I) {
+      r_eff[i] <- rhyper(1, n_s, n_e + n_i + n_r - 1, r_i[i])
+      n_s <- n_s - r_eff[i]
+      n_e <- n_e + r_eff[i]
+    }
   } else {
-    return(delta)
+    r_eff <- 0
   }
+  return(r_eff)
 }
 
 # This function caluclates summary stats about secondary cases
@@ -103,28 +103,6 @@ summarize_dgp <- function(x) {
     "q3" = quantile(x, 0.75, names = F),
     "sd" = sd(x)
   )
-}
-
-sim_infections <- function(N, S, I, R) {
-  if (I != 0) {
-    r_i <- rpois(I, rgamma(I, 0.16, 0.16 / 2.5))
-    r_i <- ifelse(r_i > N-1, N-1, r_i)
-    
-    susceptible <- S
-    exposed <- 0
-    infected <- I
-    recovered <- R
-    r_new <- vector()
-    
-    for (i in 1:length(r_i)) {
-      r_new[i] <- rhyper(1, susceptible, exposed + infected + recovered - 1, r_i[i])
-      susceptible <- susceptible - r_new[i]
-      exposed <- exposed + r_new[i]
-    }
-  } else {
-    r_new <- 0
-  }
-  return(r_new)
 }
 
 
