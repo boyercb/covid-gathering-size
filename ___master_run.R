56e972afe12cb1be43d9430a67b31f015ad7dac5
Christopher Boyer <christopherbboyer@gmail.com>
2022-04-14 20:47:46 -0400

    Update ___master_run.R

--- ___master_run.R
+++ ___master_run.R
@@ -1,17 +1,14 @@
-library(tidyverse)
-library(deSolve)
-library(rmutil)
-library(hexbin)
-library(gridExtra)
-library(RColorBrewer)
+source('2_code/0_packages.R')
 
 # global parameters -------------------------------------------------------
 
+# probably don't need.
 SIMS <- 100000
 rerun_simulation <- TRUE
 
 # load helper functions ---------------------------------------------------
 
+# figure out if we need any of these
 source("0_functions/simulation_functions.R")
 
 source("0_functions/plot_functions.R")
@@ -21,20 +18,9 @@ source("0_functions/alternative_model.R")
 
 # run code ----------------------------------------------------------------
 
-source("2_code/1_load_bbc_data.R")
+source("2_code/1_theory.R")
 
-source("2_code/2_heterogeneity_in_q.R")
+source("2_code/2_empirical_distributions.R")
 
-source("2_code/3_heterogeneity_in_S.R")
+source("2_code/3_empirical_restrictions.R")
 
-source("2_code/4_lorenz_curves.R")
-
-source("2_code/5_unconstrained_outbreak.R")
-
-source("2_code/6_how_low_to_set_c.R")
-
-source("2_code/7_how_long_to_hold.R")
-
-source("2_code/8_time_dependent_interventions.R")
-
-source("2_code/9_sensitivity_analyses.R")
