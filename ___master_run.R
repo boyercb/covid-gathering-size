0341692153ceee2160205d07d9298072820b4816
Christopher Boyer <christopherbboyer@gmail.com>
2022-04-27 19:51:08 -0400

    Update ___master_run.R

--- ___master_run.R
+++ ___master_run.R
@@ -1,11 +1,5 @@
 source('2_code/0_packages.R')
 
-# global parameters -------------------------------------------------------
-
-# probably don't need.
-SIMS <- 100000
-rerun_simulation <- TRUE
-
 # load helper functions ---------------------------------------------------
 
 # figure out if we need any of these
