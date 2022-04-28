25e743a3211f8c9fc9e2d09a25a67cec13d1a869
Christopher Boyer <christopherbboyer@gmail.com>
2022-04-28 12:53:31 -0400

    Delete __archive directory

--- __archive/9_sensitivity_analyses.R
+++
@@ -1,6 +0,0 @@
-# sensitivity analysis no. 1 - one q per person, one q per day per person, q
-# distribution follows viral load over 5 days
-
-
-# sensitivity analysis no. 2 - contact draws are independent vs. perfectly
-# correlated
